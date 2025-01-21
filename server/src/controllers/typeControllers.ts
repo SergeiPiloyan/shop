87031b1e890c54ff6fd52b24def1ccfaa3f9dc23
Sergei_Piloyan <sergei.piloyan0302@gmail.com>
2025-01-21 13:44:12 -0800

    [SERRVER]: add type controllers

--- server/src/controllers/typeControllers.ts
+++ server/src/controllers/typeControllers.ts
@@ -1,7 +1,17 @@
 import { Request, Response } from "express";
+import { Type } from "../models/models";
 
 export class TypeControllers {
-  public static async create(req: Request, res: Response) {}
+  public static async create(req: Request, res: Response) {
+    const { name } = req.body;
+    const type = await Type.create({ name });
 
-  public static async getAll(req: Request, res: Response) {}
+    res.json(type);
+  }
+
+  public static async getAll(req: Request, res: Response) {
+    const types = await Type.findAll();
+
+    res.json(types);
+  }
 }
