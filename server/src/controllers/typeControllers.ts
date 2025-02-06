4a80aed34c3411bc2b669523075eac62b7e46b45
Sergei_Piloyan <sergei.piloyan0302@gmail.com>
2025-02-05 16:54:14 -0800

    [SERVER]: add try catch to type controller

--- server/src/controllers/typeControllers.ts
+++ server/src/controllers/typeControllers.ts
@@ -1,12 +1,18 @@
-import { Request, Response } from "express";
+import { NextFunction, Request, Response } from "express";
 import { Type } from "../models/models";
+import { ApiError } from "../error/ApiError";
+import { ErrorCode } from "../enums/ErrorCodes";
 
 export class TypeControllers {
-  public static async create(req: Request, res: Response) {
-    const { name } = req.body;
-    const type = await Type.create({ name });
+  public static async create(req: Request, res: Response, next: NextFunction) {
+    try {
+      const { name } = req.body;
+      const type = await Type.create({ name });
 
-    res.json(type);
+      res.json(type);
+    } catch (error) {
+      return next(ApiError.handleError(ErrorCode.BAD_REQUEST));
+    }
   }
 
   public static async getAll(req: Request, res: Response) {
